f376ca8ce7c5a6b4d88a87232e10abe27d44964e
piroshi1989 <hiroshi19890606@gmail.com>
2023-06-16 09:07:41 +0900

    勤怠管理ページ完成

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -3,8 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Attendance;
+use Carbon\Carbon;
+use Illuminate\Pagination\Paginator;
 
 class AttendanceController extends Controller
 {
-    //
+    public function attendanceView(){
+
+        $attendanceRecords = Attendance::with('breakTimes')->latest()->get();
+        //昇順に並べ替えて取得
+
+        $groupedRecords = $attendanceRecords->groupBy(function ($record) {
+        $carbonDate = Carbon::parse($record->start_time);
+            return $carbonDate->format('Y-m-d');
+        });
+        
+        $formattedRecords = $groupedRecords->map(function ($records, $date) {
+            return [
+                'date' => $date,
+                'records' => $records->map(function ($record) {
+                    $start = Carbon::parse($record->start_time);
+                    $end = Carbon::parse($record->end_time);
+                    //勤務の開始時間、終了時間のインスタンスを作成
+
+                    $totalBreakTime = $record->breakTimes->sum(function ($breakTime) {
+                    $start = Carbon::parse($breakTime->start_time);
+                    $end = Carbon::parse($breakTime->end_time);
+                        return $start->diffInSeconds($end);
+                    });
+                     //休憩の開始時間、終了時間のインスタンスを作成し、差分を秒で返す
+
+                    $totalAttendanceTime = $end->diffInSeconds($start) - $totalBreakTime;
+                    //勤務終了と開始の差分を求め、そこから休憩時間を引く
+                    
+                    $hours = floor($totalBreakTime / 3600);
+                    $minutes = floor(($totalBreakTime % 3600) / 60);
+                    $seconds = $totalBreakTime % 60;
+                    //差分は数値になっているため00:00:00の形に整える
+                    $formattedTotalBreakTime = sprintf ('%02d:%02d:%02d', $hours, $minutes, $seconds);
+
+                    $hours = floor($totalAttendanceTime / 3600);
+                    $minutes = floor(($totalAttendanceTime % 3600) / 60);
+                    $seconds = $totalAttendanceTime % 60;
+                    //差分は数値になっているため00:00:00の形に整える
+                    $formattedTotalAttendanceTime = sprintf ('%02d:%02d:%02d', $hours, $minutes, $seconds);
+            
+                    return [
+                        'name' => $record->user->name,
+                        'start_time' => Carbon::parse($record->start_time)->format('h:i:s'),
+                        'end_time' => Carbon::parse($record->end_time)->format('h:i:s'),
+                        'total_break_time' => $formattedTotalBreakTime,
+                        'total_attendance_time'=> $formattedTotalAttendanceTime,
+                        // 他の属性のフォーマットも追加
+            ];
+        })->paginate($perPage = 5, $pageName = 'records'),
+    ];
+})->simplePaginate(
+    $perPage = 1, $pageName = 'dates');
+
+
+    
+    return view('attendance', compact('formattedRecords'));
+}
 }
--- src/resources/views/attendance.blade.php
+++ src/resources/views/attendance.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
+
+
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/attendance.css') }}">
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 @endsection
 
 @section('content')
 <div class="date-table">
     <div class="date-table__heading">
-        <h2>2021-11-01</h2>
-        {{-- あとで修正 ここは、datetimeのformat修正して$date[]とかにする？ --}}
-        {{-- ページネート？？ --}}
+        @foreach ($formattedRecords as $data)
+        <h2>{{ $data['date'] }}</h2>
+{{ $formattedRecords->links('vendor.pagination.simple-tailwind') }}
     </div>
     <table class="date-table__inner">
         <tr class="date-table__row">
@@ -18,15 +21,17 @@
             <th class="date-table__header">休憩時間</th>
             <th class="date-table__header">勤務時間</th>
         </tr>
+            @foreach ($data['records'] as $record)
         <tr class="date-table__row">
-            <td class="date-table__item">テスト太郎</td>
-            <td class="date-table__item">10:00:00</td>
-            <td class="date-table__item">20:00:00</td>
-            <td class="date-table__item">00:30:00</td>
-            <td class="date-table__item">09:30:00</td>
-            {{-- ここはあとでforeach入れる --}}
+            <td class="date-table__item">{{ $record['name'] }}</td>
+            <td class="date-table__item">{{ $record['start_time']}}</td>
+            <td class="date-table__item">{{ $record['end_time']}}</td>
+            <td class="date-table__item">{{ $record['total_break_time'] }}</td>
+            <td class="date-table__item">{{ $record['total_attendance_time'] }}</td>
+            @endforeach
+        @endforeach
         </tr>
     </table>
-    {{-- ページネートする --}}
+        {{ $data['records']->links('vendor.pagination.tailwind') }}
 </div>
 @endsection
